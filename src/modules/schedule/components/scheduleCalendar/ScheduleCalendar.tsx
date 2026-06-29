7f1ff8ed66a98433205b5876663890c9dc442c4e
nikshep <klimenko.leva.lol2@yandex.ru>
2026-06-30 00:51:34 +0800

    feat: add mobile and tablet version

--- src/modules/schedule/components/scheduleCalendar/ScheduleCalendar.tsx
+++ src/modules/schedule/components/scheduleCalendar/ScheduleCalendar.tsx
@@ -1,17 +1,21 @@
 import { MonthCalendar } from "@/modules/schedule/components/scheduleCalendar/MonthCalendar";
-
+import { WeekCalendar } from "@/modules/schedule/components/scheduleCalendar/WeekCalendar";
+import type { CalendarView } from "@/modules/schedule/lib/calendar";
 import type { ScheduleEvent } from "@/modules/schedule/model/types";
 
 export type ScheduleCalendarProps = {
   days: Date[];
   calendarDate: Date;
-
+  view: CalendarView;
   eventsByDate: Record<string, ScheduleEvent[]>;
   onEventClick: (event: ScheduleEvent) => void;
-
   onMoreEventsClick?: (date: string) => void;
 };
 
 export const ScheduleCalendar = (props: ScheduleCalendarProps) => {
+  if (props.view === "week") {
+    return <WeekCalendar {...props} />;
+  }
+
   return <MonthCalendar {...props} />;
 };
